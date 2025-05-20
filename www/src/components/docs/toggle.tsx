0321353b312bf00554ed9888f7555fa4ad08fddb
akshaytheGodxo <akshaysinghc230@gmail.com>
2025-05-20 23:02:57 +0530

    integrated buttons and stuff

--- www/src/components/docs/toggle.tsx
+++ www/src/components/docs/toggle.tsx
@@ -1,36 +1,31 @@
 import { useState } from "react";
-import { createRoot } from "react-dom/client";
 
-import FolderStructureApp from "../../pages/en/folder-structure-app.mdx";
-import FolderStructurePages from "../../pages/en/folder-structure-pages.mdx";
+interface FolderStructureApp {
+  currentState: string;
+  setCurrentState: (state: string) => void;
+}
 
-export default function Toggle() {
-  const [isAppRouterSelected, setIsAppRouterSelected] = useState(true);
-  console.log("what", FolderStructureApp);
+export default function ToggleButton({
+  currentState,
+  setCurrentState,
+}: FolderStructureApp) {
+  console.log("Selected:", currentState);
   return (
     <>
       <div className="flex w-full justify-center gap-10 py-5">
         <button
-          className={`${isAppRouterSelected ? "bg-primary text-white" : "bg-transparent"} inline-flex cursor-pointer items-center rounded-lg px-4 py-2 text-sm font-semibold transition-colors duration-500 hover:no-underline md:px-5 md:text-base lg:px-4 lg:py-3`}
-          onClick={() => setIsAppRouterSelected(true)}
+          className={`${currentState ? "bg-primary text-white" : "bg-transparent"} inline-flex cursor-pointer items-center rounded-lg px-4 py-2 text-sm font-semibold transition-colors duration-500 hover:no-underline md:px-5 md:text-base lg:px-4 lg:py-3`}
+          onClick={() => setCurrentState("app")}
         >
           App Router
         </button>
         <button
-          className={`${!isAppRouterSelected ? "bg-primary text-white" : "bg-transparent"} inline-flex cursor-pointer items-center rounded-lg px-4 py-2 text-sm font-semibold transition-colors duration-500 hover:no-underline md:px-5 md:text-base lg:px-4 lg:py-3`}
-          onClick={() => setIsAppRouterSelected(false)}
+          className={`${!currentState ? "bg-primary text-white" : "bg-transparent"} inline-flex cursor-pointer items-center rounded-lg px-4 py-2 text-sm font-semibold transition-colors duration-500 hover:no-underline md:px-5 md:text-base lg:px-4 lg:py-3`}
+          onClick={() => setCurrentState("pages")}
         >
           {" "}
           Pages Router
         </button>
-      </div>
-
-      <div>
-        {isAppRouterSelected ? (
-          <FolderStructureApp />
-        ) : (
-          <FolderStructurePages />
-        )}
       </div>
     </>
   );
